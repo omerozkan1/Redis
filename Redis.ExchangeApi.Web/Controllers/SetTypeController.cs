de8003ea20a718da54445f545060a5c26ddb4a53
omer <gs_omer_ozkan@hotmail.com>
2021-02-15 17:09:03 +0300

    Examples with redis Set and Sorted Set.

---
+++ Redis.ExchangeApi.Web/Controllers/SetTypeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Redis.ExchangeApi.Web.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Redis.ExchangeApi.Web.Controllers
+{
+    public class SetTypeController : Controller
+    {
+        private readonly RedisService redisService;
+        private readonly IDatabase db;
+        private string listKey = "setNames";
+        public SetTypeController(RedisService redisService)
+        {
+            this.redisService = redisService;
+            db = redisService.GetDb(2);
+        }
+        public IActionResult Index()
+        {
+            HashSet<string> namesList = new HashSet<string>();
+            if (db.KeyExists(listKey))
+            {
+                db.SetMembers(listKey).ToList().ForEach(x =>
+                {
+                    namesList.Add(x.ToString());
+                });
+            }
+            return View(namesList);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            db.KeyExpire(listKey, DateTime.Now.AddMinutes(5));         
+            db.SetAdd(listKey, name);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> DeleteItem(string name)
+        {
+            await db.SetRemoveAsync(listKey, name);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Redis.ExchangeApi.Web/Controllers/SortedSetTypeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Redis.ExchangeApi.Web.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Redis.ExchangeApi.Web.Controllers
+{
+    public class SortedSetTypeController : Controller
+    {
+        private readonly RedisService redisService;
+        private readonly IDatabase db;
+        private string listKey = "sortedSetNames";
+        public SortedSetTypeController(RedisService redisService)
+        {
+            this.redisService = redisService;
+            db = redisService.GetDb(3);
+        }
+        public IActionResult Index()
+        {
+            HashSet<string> list = new HashSet<string>();
+            if (db.KeyExists(listKey))
+            {
+                //db.SortedSetScan(listKey).ToList().ForEach(x=> 
+                //{
+                //    list.Add(x.ToString());
+                //});
+                db.SortedSetRangeByRank(listKey, order: Order.Descending).ToList().ForEach(x =>
+                {
+                    list.Add(x.ToString());
+                });
+            }
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name, int score)
+        {
+            db.SortedSetAdd(listKey, name, score);
+            db.KeyExpire(listKey, DateTime.Now.AddMinutes(1));
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteItem(string name)
+        {
+            db.SortedSetRemove(listKey, name);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
