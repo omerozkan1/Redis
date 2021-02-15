aa9da596ad6042a713bd379124d1e588c8034caf
omer <gs_omer_ozkan@hotmail.com>
2021-02-15 14:55:22 +0300

    Examples with redis string.

---
+++ Redis.ExchangeApi.Web/Controllers/StringTypeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Redis.ExchangeApi.Web.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Redis.ExchangeApi.Web.Controllers
+{
+    public class StringTypeController : Controller
+    {
+        private readonly RedisService redisService;
+        private readonly IDatabase db;
+        public StringTypeController(RedisService redisService)
+        {
+            this.redisService = redisService;
+            db = redisService.GetDb(0);
+        }
+        public IActionResult Index()
+        {            
+            db.StringSet("name", "Omer Ozkan");
+            db.StringSet("ziyaretci", 100);
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            var redisValue = db.StringLength("name");
+            //var redisValue = db.StringGetRange("name",0,2);
+            //db.StringIncrement("ziyaretci", 1);
+
+            db.StringDecrementAsync("ziyaretci", 10).Wait();
+            ViewBag.Value = redisValue.ToString();
+
+            //if (redisValue.HasValue)
+            //{
+            //    ViewBag.Value = redisValue.ToString();
+            //}
+
+            return View();
+        }
+
+    }
+}
